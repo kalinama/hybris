b2739a95dcb061659bba4fcca55a52c374afb653
kalinama <margoageenko@gmail.com>
2021-02-06 01:20:50 +0300

    Define Default Property Value

--- bin/custom/concerttours/src/concerttours/populators/BandPopulator.java
+++ bin/custom/concerttours/src/concerttours/populators/BandPopulator.java
@@ -4,18 +4,25 @@ import concerttours.data.BandData;
 import concerttours.enums.MusicType;
 import concerttours.model.BandModel;
 import de.hybris.platform.converters.Populator;
+import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
 import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Required;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BandPopulator implements Populator<BandModel, BandData> {
 
+    public static final String BAND_NAME_PREFIX = "concerttours.band.name.prefix";
+
+    private ConfigurationService configurationService;
+
     @Override
     public void populate(BandModel bandModel, BandData bandData) throws ConversionException {
+        String prefix = configurationService.getConfiguration().getString(BAND_NAME_PREFIX);
         bandData.setId(bandModel.getCode());
-        bandData.setName(bandModel.getName());
+        bandData.setName(prefix + bandModel.getName());
         bandData.setDescription(bandModel.getHistory());
         bandData.setAlbumsSold(bandModel.getAlbumSales());
         bandData.setPlayers(bandModel.getPlayersInPopularityOrder());
@@ -25,5 +32,10 @@ public class BandPopulator implements Populator<BandModel, BandData> {
                 .map(MusicType::getCode)
                 .collect(Collectors.toList());
         bandData.setGenres(genres);
+    }
+
+    @Required
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
     }
 }
