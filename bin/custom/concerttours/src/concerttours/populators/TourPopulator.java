3e3f63462ad63a7c16af2c652d674e655bc1b0df
kalinama <margoageenko@gmail.com>
2021-02-15 17:04:11 +0300

    Add Producer Model

--- bin/custom/concerttours/src/concerttours/populators/TourPopulator.java
+++ bin/custom/concerttours/src/concerttours/populators/TourPopulator.java
@@ -1,9 +1,12 @@
 package concerttours.populators;
 
 import concerttours.data.TourData;
+import concerttours.model.ProducerModel;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+
+import java.util.Optional;
 
 public class TourPopulator implements Populator<ProductModel, TourData> {
     @Override
@@ -11,5 +14,8 @@ public class TourPopulator implements Populator<ProductModel, TourData> {
         tourData.setId(productModel.getCode());
         tourData.setTourName(productModel.getName());
         tourData.setDescription(productModel.getDescription());
+
+        Optional.ofNullable(productModel.getProducer())
+                .ifPresent(producer -> tourData.setProducer(producer.getFullName()));
     }
 }
--- bin/custom/concerttours/web/webroot/WEB-INF/views/tourDetails.jsp
+++ bin/custom/concerttours/web/webroot/WEB-INF/views/tourDetails.jsp
@@ -7,6 +7,7 @@
 <div>
     <h1>${tour.tourName} </h1>
     <h2>${tour.description}</h2>
+    <h3>Producer: ${tour.producer}</h3>
 
     <c:forEach items="${tour.concerts}" var="concert">
         <a href="${pageContext.request.contextPath}/concerts/${concert.id}"> ${concert.venue}</a>
